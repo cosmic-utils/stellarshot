092f3fadc5be0bd3929a50ca4a8182f099170817
Eduardo Flores <edfloreshz@gmail.com>
2024-05-18 14:06:52 -0700

    chore: add config

---
+++ src/app/config.rs
@@ -0,0 +1,52 @@
+use crate::app::App;
+use cosmic::{
+    cosmic_config::{self, cosmic_config_derive::CosmicConfigEntry, Config, CosmicConfigEntry},
+    theme, Application,
+};
+use serde::{Deserialize, Serialize};
+
+pub const CONFIG_VERSION: u64 = 1;
+
+#[derive(Clone, Default, Debug, Eq, PartialEq, Deserialize, Serialize, CosmicConfigEntry)]
+pub struct CosmicBackupsConfig {
+    pub app_theme: AppTheme,
+}
+
+impl CosmicBackupsConfig {
+    pub fn config_handler() -> Option<Config> {
+        Config::new(App::APP_ID, CONFIG_VERSION).ok()
+    }
+
+    pub fn config() -> CosmicBackupsConfig {
+        match Self::config_handler() {
+            Some(config_handler) => {
+                let config = CosmicBackupsConfig::get_entry(&config_handler).unwrap_or_else(
+                    |(errs, config)| {
+                        log::info!("errors loading config: {:?}", errs);
+                        config
+                    },
+                );
+                config
+            }
+            None => CosmicBackupsConfig::default(),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
+pub enum AppTheme {
+    Dark,
+    Light,
+    #[default]
+    System,
+}
+
+impl AppTheme {
+    pub fn theme(&self) -> theme::Theme {
+        match self {
+            Self::Dark => theme::Theme::dark(),
+            Self::Light => theme::Theme::light(),
+            Self::System => theme::system_preference(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use crate::app::App;
+use app::{config::CosmicBackupsConfig, Flags};
+use cosmic::app::Settings;
+
 /// The `app` module is used by convention to indicate the main component of our application.
 mod app;
 mod core;
@@ -11,6 +14,15 @@ mod core;
 /// - `()` is the flags that your app needs to use before it starts.
 ///  If your app does not need any flags, you can pass in `()`.
 fn main() -> cosmic::iced::Result {
-    let settings = cosmic::app::Settings::default();
-    cosmic::app::run::<App>(settings, ())
+    let (settings, flags) = settings();
+    cosmic::app::run::<App>(settings, flags)
+}
+
+fn settings() -> (Settings, Flags) {
+    let settings = Settings::default();
+    let flags = Flags {
+        config_handler: CosmicBackupsConfig::config_handler(),
+        config: CosmicBackupsConfig::config(),
+    };
+    (settings, flags)
 }
