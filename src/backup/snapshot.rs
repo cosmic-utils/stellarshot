c7e6ace2b785ac6cc2ddb7a245839a5df608ede7
Eduardo Flores <edfloreshz@gmail.com>
2024-05-26 19:38:50 -0700

    feat: add method to fetch snapshots

--- src/backup/snapshot.rs
+++ src/backup/snapshot.rs
@@ -1,5 +1,7 @@
 use rustic_backend::BackendOptions;
-use rustic_core::{BackupOptions, PathList, Repository, RepositoryOptions, SnapshotOptions};
+use rustic_core::{
+    repofile::SnapshotFile, BackupOptions, PathList, Repository, RepositoryOptions, SnapshotOptions,
+};
 use std::error::Error;
 
 pub fn snapshot(repository: &str, password: &str, paths: Vec<&str>) -> Result<(), Box<dyn Error>> {
@@ -33,6 +35,24 @@ pub fn snapshot(repository: &str, password: &str, paths: Vec<&str>) -> Result<()
 
     println!("successfully created snapshot:\n{snap:#?}");
     Ok(())
+}
+
+pub fn snapshots(repository: &str, password: &str) -> Result<Vec<SnapshotFile>, Box<dyn Error>> {
+    let backends = BackendOptions::default()
+        .repository(repository)
+        .to_backends()?;
+
+    println!("successfully initialized backends:\n{backends:#?}");
+
+    let repo_opts = RepositoryOptions::default().password(password);
+
+    let repo = Repository::new(&repo_opts, backends)?
+        .open()?
+        .to_indexed_ids()?;
+
+    let snapshots = repo.get_all_snapshots()?;
+
+    Ok(snapshots)
 }
 
 #[cfg(test)]
